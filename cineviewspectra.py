93da090ed392889b09033c1bf584241439834505
Sylvie Dagoret-Campagne <dagoret@lal.in2p3.fr>
2019-04-25 15:51:35 +0200

    add cinema

---
+++ cineviewspectra.py
@@ -0,0 +1,151 @@
+#########################################################################################################
+# View spectra produced in Spectractor
+#########################################################################################################
+
+import os
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+import re
+
+import matplotlib.pyplot as plt
+
+if not 'workbookDir' in globals():
+    workbookDir = os.getcwd()
+print('workbookDir: ' + workbookDir)
+
+spectractordir=workbookDir+"/../Spectractor"
+print('spectractordir: ' + spectractordir)
+
+import sys
+sys.path.append(workbookDir)
+sys.path.append(spectractordir)
+sys.path.append(os.path.dirname(workbookDir))
+
+from spectractor import parameters
+from spectractor.extractor.extractor import Spectractor
+from spectractor.logbook import LogBook
+from spectractor.extractor.dispersers import *
+from spectractor.extractor.spectrum import *
+
+plt.rcParams["figure.figsize"] = (20,10)
+
+if __name__ == "__main__":
+
+    #####################
+    # 1) Configuration
+    ######################
+
+    parameters.VERBOSE = True
+    parameters.DEBUG = True
+
+    #thedate="20190214"
+    thedate = "20190215"
+
+    #output_directory = "output/" + thedate
+    #output_directory = "/Users/dagoret/DATA/PicDuMidiFev2019/spectractor_spectra/" + thedate
+    output_directory = "/Users/dagoret/DATA/PicDuMidiFev2019/spectractor_output/"+ thedate
+
+    parameters.VERBOSE = True
+    parameters.DISPLAY = True
+
+
+
+
+    ############################
+    # 2) Get the config
+    #########################
+
+    config = spectractordir+"/config/picdumidi.ini"
+
+    my_logger = set_logger(__name__)
+    my_logger.info('\n\tStart RunViewSpectra')
+    # Load config file
+    load_config(config)
+
+
+
+    ############################
+    # 3) Get Spectra filelist
+    #########################
+
+
+    # get all files
+    onlyfiles = [f for f in listdir(output_directory) if isfile(join(output_directory, f))]
+    onlyfiles = np.array(onlyfiles)
+
+    # sort files
+    sortedindexes = np.argsort(onlyfiles)
+    onlyfiles = onlyfiles[sortedindexes]
+
+    # get only _spectrum.fits file
+    onlyfilesspectrum = []
+    for f in onlyfiles:
+        if re.search("^T.*_spectrum.fits$", f):
+            onlyfilesspectrum.append(re.findall("(^T.*_spectrum.fits$)", f)[0])
+
+
+
+    onlyfilesspectrum = np.array(onlyfilesspectrum)
+    sortedindexes = np.argsort(onlyfilesspectrum)
+    onlyfilesspectrum = onlyfilesspectrum[sortedindexes]
+
+
+    #get basnemae of files for later use (to check if _table.csv and _spectrogram.fits exists
+    onlyfilesbasename=[]
+    for f in onlyfilesspectrum:
+        onlyfilesbasename.append(re.findall("(^T.*)_spectrum.fits$",f)[0])
+
+
+    basenamecut=[]
+    for f in onlyfilesspectrum:
+        basenamecut.append(f.split("_HD")[0])
+
+
+    #############################################
+    # 3) Plot Spectra
+    ##########################################
+
+    NBSPEC = len(sortedindexes)
+
+    plt.ion()
+
+
+    for idx in np.arange(0, NBSPEC):
+        # if idx in [0,1,4]:
+        #    continue
+
+        print("{}) : {}".format(idx,onlyfilesspectrum[idx]))
+
+        fullfilename = os.path.join(output_directory, onlyfilesspectrum[idx])
+        s = Spectrum()
+        s.load_spectrum(fullfilename)
+        am=s.header["AIRMASS"]
+
+        labelname=basenamecut[idx]+" :: Z = {:1.2f}".format(am)
+
+        #fig=plt.figure(figsize=[12, 6])
+        ax = plt.gca()
+
+        s.plot_spectrum(ax=ax,xlim=None, label=labelname,force_lines=True)
+        plt.ylim(0,5e-11)
+
+        plt.draw()
+        plt.pause(0.001)
+        plt.clf()
+
+        #figfilename="figures/20190215/fig_spec_"+basenamecut[idx]+".png"
+        #fig.savefig(figfilename)
+
+
+
+
+
+
+
+
+
+
+
+
+
